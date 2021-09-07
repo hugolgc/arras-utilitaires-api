fbbf3a8871ea18e2980d27bb49860318d5d0f43d
hugolgc <lagache.hugo@hotmail.fr>
2021-09-07 12:27:45 +0200

    Delete rolling

--- config/database.js
+++ config/database.js
@@ -1,22 +1,3 @@
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'bookshelf',
-//       settings: {
-//         client: 'mysql',
-//         host: env('DATABASE_HOST', 'localhost'),
-//         port: env.int('DATABASE_PORT', 3306),
-//         database: env('DATABASE_NAME', 'master'),
-//         username: env('DATABASE_USERNAME', 'root'),
-//         password: env('DATABASE_PASSWORD', 'root'),
-//         ssl: env.bool('DATABASE_SSL', false),
-//       },
-//       options: {}
-//     },
-//   },
-// });
-
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
@@ -24,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', 'remotemysql.com'),
+        host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'OH5gMg7P5G'),
-        username: env('DATABASE_USERNAME', 'OH5gMg7P5G'),
-        password: env('DATABASE_PASSWORD', 'fU6NzI364G'),
+        database: env('DATABASE_NAME', 'master'),
+        username: env('DATABASE_USERNAME', 'root'),
+        password: env('DATABASE_PASSWORD', 'root'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
